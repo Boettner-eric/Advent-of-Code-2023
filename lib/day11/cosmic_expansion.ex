e105bfd564f109e1f5ae030c1882236e64bd5dc2
Eric Boettner <boettner.eric@gmail.com>
2023-12-11 10:36:00 -0800

    sol: day 11

---
+++ lib/day11/cosmic_expansion.ex
@@ -0,0 +1,55 @@
+defmodule CosmicExpansion do
+  def problem_one(filename \\ "lib/day11/input.txt") do
+    Aoc23.read_lines(filename)
+    |> Enum.map(&String.split(&1, "", trim: true))
+    |> get_points()
+  end
+
+  def get_points(space, size \\ 2) do
+    points =
+      for y <- 0..(length(space) - 1),
+          x <- 0..(length(Enum.at(space, 0, 0)) - 1),
+          reduce: [] do
+        acc ->
+          if Enum.at(Enum.at(space, y), x) == "#", do: [{x, y} | acc], else: acc
+      end
+
+    columns =
+      Enum.reduce(0..(length(space) - 1), [], fn i, acc ->
+        if Enum.any?(points, &(elem(&1, 0) == i)), do: acc, else: [i | acc]
+      end)
+
+    rows =
+      Enum.reduce(0..(length(Enum.at(space, 0, 0)) - 1), [], fn j, acc ->
+        if Enum.any?(points, &(elem(&1, 1) == j)), do: acc, else: [j | acc]
+      end)
+
+    # add column offsets
+    points =
+      Enum.reduce(columns, points, fn i, acc ->
+        Enum.map(acc, fn {x, y} -> if x > i, do: {x + size - 1, y}, else: {x, y} end)
+      end)
+
+    # add row offsets
+    points =
+      Enum.reduce(rows, points, fn j, acc ->
+        Enum.map(acc, fn {x, y} -> if y > j, do: {x, y + size - 1}, else: {x, y} end)
+      end)
+
+    Enum.reduce(0..(length(points) - 1), 0, fn index, acc ->
+      Enum.slice(points, (index + 1)..(length(points) - 1))
+      |> Enum.reduce(0, &(shortest_path(&1, Enum.at(points, index)) + &2))
+      |> Kernel.+(acc)
+    end)
+  end
+
+  def shortest_path({x, y}, {i, j}) do
+    abs(y - j) + abs(x - i)
+  end
+
+  def problem_two(filename \\ "lib/day11/input.txt", size \\ 2) do
+    Aoc23.read_lines(filename)
+    |> Enum.map(&String.split(&1, "", trim: true))
+    |> get_points(size)
+  end
+end
---
+++ test/days/day_eleven_test.exs
@@ -0,0 +1,23 @@
+defmodule DayElevenTest do
+  use ExUnit.Case
+
+  test "day eleven, gets correct answer for problem one" do
+    assert CosmicExpansion.problem_one() == 9_418_609
+  end
+
+  test "day eleven, gets correct answer for sample problem one" do
+    assert CosmicExpansion.problem_one("lib/day11/sample.txt") == 374
+  end
+
+  test "day ten, gets correct answer for problem two" do
+    assert CosmicExpansion.problem_two("lib/day11/input.txt", 1_000_000) == 593_821_230_983
+  end
+
+  test "day ten, gets correct answer for sample two (10x)" do
+    assert CosmicExpansion.problem_two("lib/day11/sample.txt", 10) == 1030
+  end
+
+  test "day ten, gets correct answer for sample two (100x)" do
+    assert CosmicExpansion.problem_two("lib/day11/sample.txt", 100) == 8410
+  end
+end
